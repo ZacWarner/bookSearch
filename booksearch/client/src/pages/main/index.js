5f83d1542d7da0e5a02b3bfe49e64e66b11d3119
ZacWarner <zektgn@gmail.com>
2019-07-28 18:24:57 -0700

    started set up for server app

---
+++ booksearch/client/src/pages/main/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+
+class Main extends Component {
+
+    render() {
+        return (
+            <Container>
+                <h6>Test!</h6>
+            </Container>
+
+        )
+    }
+}
+
+export default Main;
